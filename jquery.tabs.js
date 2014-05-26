2f74a52ee22f699c72b206b48214c743bbb25964
Shohei Yamasaki <shyamasaki@gmail.com>
2014-05-26 22:58:34 +0900

    jQuery.fn.extendで拡張する

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -4,24 +4,25 @@
  * https://github.com/shoyan/jquery.tabs.js
  */
 ;(function($) {
-
   'use strict';
 
-  $.fn.tabs= function() {
-    var target = $(this).find('a');
+  $.fn.extend({
+    tabs: function() {
+      var target = $(this).find('a');
 
-    target.on('click', function(e) {
-      e.preventDefault();
-      var $this = $(this),
-      tabID = $(this).data('target');
+      target.on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this),
+        tabID = $(this).data('target');
 
-      $(tabID).siblings().removeClass('active')
-      $(tabID).addClass('active');
+        $(tabID).siblings().removeClass('active')
+        $(tabID).addClass('active');
 
-      $this.parent('li').siblings().children().removeClass('active');
-      $this.addClass('active');
-      return true;
-    });
-  }
+        $this.parent('li').siblings().children().removeClass('active');
+        $this.addClass('active');
+        return true;
+      });
+    }
+  });
 })(jQuery);
 
